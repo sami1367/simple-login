74fdba796da481c5c8ab1726575beb3b295bce4c
sami rahimi <sami@localhost.localdomain>
2019-05-12 18:31:25 +0430

    modify index pages  Changes to be committed: 	modified:   index.php

--- index.php
+++ index.php
@@ -18,7 +18,68 @@
 </head>
 <body>
 
+<form id="regForm" action="/action_page.php">
+  <h1>Login or Register</h1>
+  <!-- One "tab" for each step in the form: -->
+  <div class="tab">
+    <p>Login:</p>
+    <p><input placeholder="Username..." name="uname"></p>
+    <p><input placeholder="Password..." name="pword" type="password"></p>
+    <div style="overflow:auto;">
+        <div style="float:right;">
+            <button type="button" id="nextBtn" onclick="">Login</button>
+            or
+            <button type="button" id="prevBtn" onclick="nextPrev(1)">Register now</button>
+        </div>
+    </div>
+  </div>
+  <div class="tab">
+    <p>insert your email address :</p>
+    <p><input type="email" placeholder="example@example.com" name="email" required></p>
+    <p style="color: red;" id="demo"></p>
+    <div style="overflow:auto;">
+        <div style="float:right;">
+            <button type="button" id="nextBtn" onclick="nextPrev(-1)">Login</button>
+            or
+            <button type="button" id="prevBtn" onclick="get_otp(email.value);">Register nowwwwwww</button>
+        </div>
+    </div>
+  </div>
+  <div class="tab">
+    <p>insert verification code :</p>
+    <p><input placeholder="******" name="code" required></p>
+    <p>code sent to : <b style="color: green;" id="emailval"></b></p>
+    <p style="color: red;" id="demo2"></p>
+    <div style="overflow:auto;">
+        <div style="float:right;">
+            <button id="myBtn" class="btnDisable" disabled type="button" onclick="nextPrev(-1)"><b id="myTimer"></b> &nbsp;&nbsp;resend email</button>
+            or
+            <button type="button" id="prevBtn" onclick="check_otp(email.value,code.value);">Submit</button>
+        </div>
+    </div>
+  </div>
+  <div class="tab">
+    <p>Login:</p>
+    <p><input placeholder="Username..." name="uname"></p>
+    <p><input placeholder="Password..." name="pword" type="password"></p>
+    <div style="overflow:auto;">
+        <div style="float:right;">
+            <button type="button" id="prevBtn" onclick="nextPrev(1)">Register now</button>
+        </div>
+    </div>
+  </div>
+  <!-- Circles which indicates the steps of the form: -->
+  <div style="text-align:center;margin-top:40px;">
+    <span class="step"></span>
+    <span class="step"></span>
+    <span class="step"></span>
+    <span class="step"></span>
+  </div>
+</form>
+
 
 
 </body>
+<!-- our JavaScript -->
+<script src="assets/index.js"></script>
 </html> 
